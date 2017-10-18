4c688ed8810a53bcf646ba2bb4a81cdbe0afa4d0
ninja247 <andrewericoleman@gmail.com>
2017-10-17 21:18:18 -0500

    stopping point for the homework

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,16 +5,15 @@ class Portfolio < ApplicationRecord
     where(subtitle: 'Angular')
   end
 
-
- scope :ruby_on_rails_portfolio_items, ->{ where(subtitle: 'Ruby On Rails')}
+# scope :ruby_on_rails_portfolio_items, ->{ where(subtitle: 'Ruby On Rails')}
 
 #testing password on terminal
 
 after_initialize :set_defaults
 
-def set_defaults
-  self.main_image ||= "http://via.placeholder.com/600x400"
-  self.thumb_image ||= "http://via.placeholder.com/350x200"
-end
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/600x400"
+    self.thumb_image ||= "http://via.placeholder.com/350x200"
+  end
 
 end
