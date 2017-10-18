949cda3e3cf609ec6ef4f367a142627bd6026345
ninja247 <andrewericoleman@gmail.com>
2017-10-18 11:39:50 -0500

    integrating the concerns

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,19 +1,14 @@
 class Portfolio < ApplicationRecord
+  includes Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
-
-  def self.angular
-    where(subtitle: 'Angular')
-  end
-
+    def self.angular
+      where(subtitle: 'Angular')
+    end
 # scope :ruby_on_rails_portfolio_items, ->{ where(subtitle: 'Ruby On Rails')}
 
-#testing password on terminal
-
 after_initialize :set_defaults
-
   def set_defaults
-    self.main_image ||= "http://via.placeholder.com/600x400"
-    self.thumb_image ||= "http://via.placeholder.com/350x200"
+    self.main_image ||= Placeholder.image_generator(height: '250', width: '250')
+    self.thumb_image ||= Placeholder.image_generator(height: '250', width: '250')
   end
-
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,16 @@
 class Skill < ApplicationRecord
-  validates_presence_of :title, :percent_utilized
+includes Placeholder
+validates_presence_of :title, :percent_utilized
+
+def self.angular
+    where(subtitle: 'Angular')
+  end
+
+after_initialize :set_defaults
+
+  def set_defaults
+    self.badge ||= Placeholder.image_generator(height: '250', width: '250')
+  end
+
+
 end
