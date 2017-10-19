39c7c9ff6272c29bdc18814afb9a553aa17efeea
ninja247 <andrewericoleman@gmail.com>
2017-10-19 11:55:39 -0500

    changed routes for custom login register logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, path: '', path_names:{ sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   resources :portfolios, except: [:show]
+  
   get 'portfolio/:id', to: 'portfolios#show', as:'portfolio_show'
  
 
@@ -16,3 +17,4 @@ Rails.application.routes.draw do
 
 end
 
+  
