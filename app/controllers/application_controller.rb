c91ee4fffc7b0c99f501a6721b793cb8a3981942
ninja247 <andrewericoleman@gmail.com>
2017-10-19 15:16:27 -0500

    created the devise whitelist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+include DeviseWhitelist
+
+#white listed
+#ac what is being whitelisted bt the devise controller?
+
+
+
 end
