98ef42e80eca9683b03e6c511e3092aff4935bce
ninja247 <andrewericoleman@gmail.com>
2017-10-20 14:40:35 -0500

    refactor source feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-include DeviseWhitelist
+  include DeviseWhitelist
+  include SetSource
 
 #white listed
 #ac what is being whitelisted bt the devise controller?
 
+#show andrew this sessions vs session
+  
 
 
 end
