3317585f87f27d40947c814344ca3fbef34aa359
ninja247 <andrewericoleman@gmail.com>
2017-10-23 12:56:38 -0500

    created links in the applicaiton view and added set source to the concerns

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include SetSource
   include CurrentUserConcern
+  include DefaultPageContent
 
 #white listed
 #ac what is being whitelisted bt the devise controller?
@@ -11,5 +12,6 @@ class ApplicationController < ActionController::Base
   
 #nul variable design pattern
 
+ 
 
 end
