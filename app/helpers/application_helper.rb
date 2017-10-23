8b1996319f58dd32bcf1bf7e211c5b8963e442d6
ninja247 <andrewericoleman@gmail.com>
2017-10-23 16:57:40 -0500

    implemented helper methods for login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def sample_helper
+    "<p>my helper </p>".html_safe
+  end
+
+    
+def login_helper
+  if current_user.is_a?(User)
+      link_to "Logout", destroy_user_session_path, method: :delete
+    else
+      (link_to "Register", new_user_registration_path) +
+      "<br>".html_safe +
+      (link_to "Login", new_user_session_path)
+    end
+
+
+#application helper vs partial
+#when you have a lot of logic use a helper when you pass in data use a partial  
+
+
+end
+
+
+
 end
