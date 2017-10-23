bf0431074615fdcf22b47bcf2bf71e1733f9da0f
ninja247 <andrewericoleman@gmail.com>
2017-10-23 14:56:33 -0500

    implemented css layouts and added them to the views and control

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,9 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.precompile += %w( blogs.scss )
+Rails.application.config.assets.precompile += %w( portfolio.scss )
+Rails.application.config.assets.precompile += %w( portfolios.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
