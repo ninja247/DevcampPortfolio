72d5ad9f9e4e30a7215d79a8d0a8209c641d057e
ninja247 <andrewericoleman@gmail.com>
2017-10-24 12:10:44 -0500

    we rendered index portfolio and blogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,21 +4,28 @@ module ApplicationHelper
   end
 
     
-def login_helper
-  if current_user.is_a?(User)
-      link_to "Logout", destroy_user_session_path, method: :delete
-    else
-      (link_to "Register", new_user_registration_path) +
-      "<br>".html_safe +
-      (link_to "Login", new_user_session_path)
-    end
+  def login_helper
+    if current_user.is_a?(User)
+        link_to "Logout", destroy_user_session_path, method: :delete
+      else
+        (link_to "Register", new_user_registration_path) +
+        "<br>".html_safe +
+        (link_to "Login", new_user_session_path)
+    end  
+  end
 
 
-#application helper vs partial
-#when you have a lot of logic use a helper when you pass in data use a partial  
+    #application helper vs partial
+    #when you have a lot of logic use a helper when you pass in data use a partial  
 
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} page"
+      content_tag(:p, greeting, class: "source_greeting") 
+    end  
+  end
 
-end
+# to comment cmnd forward slash
 
 
 
