1e8747b5d06a97c605763c99632720c95f2c57cc
ninja247 <andrewericoleman@gmail.com>
2017-10-27 13:06:18 -0500

    Implemented partials for blog, font awesome, ang pagination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,24 @@
 module ApplicationHelper
 
 
-  def login_helper style
+   def login_helper style = ''
     if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path, class: style) + " ".html_safe + (link_to "Login", new_user_session_path, class: style)
+      (link_to "Register", new_user_registration_path, class: style) +
+      " ".html_safe +
+      (link_to "Login", new_user_session_path, class: style)
     else
       link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
   end
-
 
     #application helper vs partial
     #when you have a lot of logic use a helper when you pass in data use a partial  
 
   def source_helper(layout_name)
     if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} page"
-      content_tag(:p, greeting, class: "source_greeting") 
-    end  
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+      content_tag(:p, greeting, class: "source-greeting")
+    end
   end
 
 # to comment cmnd forward slash
