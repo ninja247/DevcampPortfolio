57ec090ea33d65a7d34ae973a9fc78a2c9e041c4
ninja247 <andrewericoleman@gmail.com>
2017-11-02 15:48:46 -0500

    made a twitter view model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
 
   devise_for :users, path: '', path_names:{ sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   resources :portfolios, except: [:show]
-  
+
+#ac comment out the next line  
   get 'portfolio/:id', to: 'portfolios#show', as:'portfolio_show'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'tech-news', to: 'pages#tech_news'
 
 
   resources :blogs do
