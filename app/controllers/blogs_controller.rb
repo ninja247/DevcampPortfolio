efaa173e130d353615d98c4562b119b2088fb1ba
ninja247 <andrewericoleman@gmail.com>
2017-11-06 15:00:02 -0600

    Implemented the blog widget for topics

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  before_action :set_sidebar_topics, except: [:update, :create, :destroy, :toggle_status]
   layout "blog"
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :edit]}, site_admin: :all
 
@@ -83,17 +84,21 @@ class BlogsController < ApplicationController
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
+
+            # Use callbacks to share common setup or constrain
+
   def set_blog
     @blog = Blog.friendly.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
+            # Never trust parameters from the scary internet, 
+
   def blog_params
     params.require(:blog).permit(:title, :body, :topic_id)
   end
 
-
-
+  def set_sidebar_topics
+    @side_bar_topics = Topic.with_blogs
+  end
 
 end
