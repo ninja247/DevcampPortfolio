31799fb030a4dd81317bc1e9e592b7eb5cde949e
ninja247 <andrewericoleman@gmail.com>
2017-11-06 11:43:15 -0600

    Update blog scope so only admin users can see draft posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -7,7 +7,18 @@ class Blog < ApplicationRecord
 
   belongs_to :topic
 
-  has_many :comments, dependent: 
-  :destroy
+  has_many :comments, dependent: :destroy
+
+  def self.special_blogs
+    all
+  end
+
+  def self.featured_blogs
+    limit(2)
+  end
+
+  def self.recent
+    order("created_at DESC")
+  end
 
 end
