e10cec398d2207a4fe2279bd30112fcaa79e01df
ninja247 <andrewericoleman@gmail.com>
2017-11-06 14:22:40 -0600

    showing the topics

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,9 +1,9 @@
 class Blog < ApplicationRecord
-  enum status: {draft: 0, published: 1 }
+  enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  validates_presence_of :title, :body, :topic
+  validates_presence_of :title, :body, :topic_id
 
   belongs_to :topic
 
